8a3ce9e0539089f824167c6aada1bb2a4ca0c7a5
Fernanda Peters <fernandapeters@gmail.com>
2023-05-17 18:36:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ruby
+
+## Requisitos
+
+* ruby (Versão 2.7.0p0)
+* minitest
+
+## Como rodar os testes
+
+No terminal, execute os comandos:
+
+```
+ruby customer_success_balancing.rb
+```
---
+++ customer_success_balancing.rb
@@ -0,0 +1,138 @@
+require 'minitest/autorun'
+require 'timeout'
+
+class CustomerSuccessBalancing
+  def initialize(customer_success, customers, away_customer_success)
+    @customer_success = customer_success
+    @customers = customers
+    @away_customer_success = away_customer_success
+  end
+
+  # Returns the ID of the customer success with most customers
+  def execute
+    remove_unavailable_costumer_success
+    sort_cs_by(:score)
+    distribute_customers
+    sort_cs_by(:customer_count)
+    count_cs_with_max_costumers == 1 ? @customer_success[-1][:id] : 0
+  end
+
+private
+
+  def remove_unavailable_costumer_success
+    @customer_success.delete_if do |cs|
+      @away_customer_success.include? cs[:id]
+    end
+  end
+
+  def sort_cs_by(index)
+    @customer_success.sort_by! { |cs| cs[index] }
+  end
+
+  def distribute_customers
+    processed_customers = 0
+    @customer_success.each do |cs|
+      customers_by_cs = @customers.count { |customer| customer[:score] <= cs[:score] } - processed_customers
+      processed_customers += customers_by_cs
+      cs[:customer_count] = customers_by_cs
+    end
+  end
+
+  def count_cs_with_max_costumers
+    @customer_success.count{ |cs| cs[:customer_count] == @customer_success[-1][:customer_count] }
+  end
+
+end
+
+class CustomerSuccessBalancingTests < Minitest::Test
+  def test_scenario_one
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([60, 20, 95, 75]),
+      build_scores([90, 20, 70, 40, 60, 10]),
+      [2, 4]
+    )
+    assert_equal 1, balancer.execute
+  end
+
+  def test_scenario_two
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([11, 21, 31, 3, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_three
+    balancer = CustomerSuccessBalancing.new(
+      build_scores(Array(1..999)),
+      build_scores(Array.new(10000, 998)),
+      [999]
+    )
+    result = Timeout.timeout(1.0) { balancer.execute }
+    assert_equal 998, result
+  end
+
+  def test_scenario_four
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([1, 2, 3, 4, 5, 6]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_five
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 2, 3, 6, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
+    assert_equal 1, balancer.execute
+  end
+
+  def test_scenario_six
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 99, 88, 3, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [1, 3, 2]
+    )
+    assert_equal 0, balancer.execute
+  end
+
+  def test_scenario_seven
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 99, 88, 3, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [4, 5, 6]
+    )
+    assert_equal 3, balancer.execute
+  end
+
+  def test_scenario_eight
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([60, 40, 95, 75]),
+      build_scores([90, 70, 20, 40, 60, 10]),
+      [2, 4]
+    )
+    assert_equal 1, balancer.execute
+  end
+
+  def test_scenario_nine
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([10, 40, 30, 20, 15]),
+      build_scores([8, 9, 35, 36, 31, 11, 13]),
+      [5]
+    )
+    assert_equal 2, balancer.execute
+  end
+  
+
+  private
+
+  def build_scores(scores)
+    scores.map.with_index do |score, index|
+      { id: index + 1, score: score }
+    end
+  end
+end
